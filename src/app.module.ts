526e3bb2b10371a2d8d1fb581e0a0ee514d19caa
endy <7ofpentacles@gmail.com>
2022-09-25 18:22:12 +0700

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,15 @@ import { RoleModule } from './role/role.module';
 import { PermissionModule } from './permission/permission.module';
 import { AuthModule } from './auth/auth.module';
 
+const isEnable = ['dev', 'dev-local'].includes(process.env.NODE_ENV);
+console.log({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -15,10 +24,10 @@ import { AuthModule } from './auth/auth.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      autoLoadEntities: ['dev', 'dev-local'].includes(process.env.NODE_ENV),
-      logging: ['dev', 'dev-local'].includes(process.env.NODE_ENV),
+      autoLoadEntities: isEnable,
+      // logging: isEnable,
       entities: [],
-      // synchronize: true,
+      synchronize: isEnable,
     }),
     CommonModule,
     UserModule,
